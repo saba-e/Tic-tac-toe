d7142b7084abec483e348495e0bf53e39d12ba05
Saba <sabaa.eky@gmail.com>
2020-12-16 16:41:33 +0330

    add src

---
+++ src/components/Board.js
@@ -0,0 +1,148 @@
+import React, { Component } from "react";
+import Square from "./Square";
+import { Grid } from "@material-ui/core";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import { Button } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+
+
+
+
+
+
+export default class Board extends Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <Container style={{ textAlign: "center" }} >
+        
+         
+        <Grid item xs={12}>
+         
+            <ButtonBase>
+              <Button
+                
+                style={{
+                  
+                  height: "50px",
+                  marginTop: "10px",
+                  marginRight: "2px",
+                }}
+              >
+                {this.renderSquare(0)}
+              </Button>
+              <Button
+                
+                style={{
+                  
+                  height: "50px",
+                  marginTop: "10px",
+                  marginRight: "2px",
+                }}
+              >
+                {this.renderSquare(1)}
+              </Button>
+
+              <Button
+                
+                style={{
+                  
+                  height: "50px",
+                  marginTop: "10px",
+                  marginRight: "2px",
+                }}
+              >
+                {this.renderSquare(2)}
+              </Button>
+            </ButtonBase>
+            <br></br>
+            <ButtonBase>
+              <Button
+                
+                style={{
+                  height: "50px",
+                  marginRight: "2px",
+                 
+                }}
+              >
+                {this.renderSquare(3)}
+              </Button>
+              <Button
+                
+                style={{
+                  height: "50px",
+                  marginRight: "2px",
+                  marginTop: "3px",
+                  marginBottom: "3px",
+                }}
+              >
+                {this.renderSquare(4)}
+              </Button>
+
+              <Button
+                
+                
+                style={{
+                  height: "50px",
+                  marginRight: "2px",
+                 
+                  marginTop: "3px",
+                  marginBottom: "3px",
+                }}
+              >
+                {this.renderSquare(5)}
+              </Button>
+            </ButtonBase>
+            <br></br>
+            <ButtonBase>
+              <Button
+                
+                style={{
+                
+                  height: "50px",
+                  marginBottom: "10px",
+                  marginRight: "2px",
+                }}
+              >
+                {this.renderSquare(6)}
+              </Button>
+              <Button
+                
+                style={{
+                 
+                  height: "50px",
+                  marginBottom: "10px",
+                  marginRight: "2px",
+                }}
+              >
+                {this.renderSquare(7)}
+              </Button>
+
+              <Button
+                
+                style={{
+               
+                  height: "50px",
+                  marginBottom: "10px",
+                  marginRight: "2px",
+                }}
+              >
+                {this.renderSquare(8)}
+              </Button>
+            </ButtonBase>
+          
+        </Grid>
+        
+     
+      </Container>
+    );
+  }
+}
---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+
+
+
+export default function Square(props) {
+    return (
+        <Button className="square" variant="outlined" color="Secondary" onClick={props.onClick}>
+            {props.value}
+        </Button>
+    )
+}
